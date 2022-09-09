99d2b6825e1d2a3db4c7deb31c8f12c5c3f14af5
Mark S <mark.schlachter@technologyadvice.com>
2022-09-09 16:50:15 -0400

    feat: Implement automatic AWS account ID resolution

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@
 
 // Standard Library Imports
 use std::process::ExitCode;
-use std::sync::Arc;
 
 // Third Party Imports
+use atomicell::AtomicCell;
 use aws_sdk_sns::Client as SNSClient;
 use aws_sdk_sqs::model::QueueAttributeName;
 use aws_sdk_sqs::Client as SQSClient;
+use aws_sdk_sts::Client as STSClient;
 use clap;
 use easy_error::{bail, Terminator};
 use once_cell::sync::OnceCell;
@@ -23,11 +24,11 @@ pub(crate) mod types;
 
 // <editor-fold desc="// Global Statics ...">
 
-pub(crate) static CLUSTER_ENV: OnceCell<Arc<EnvName>> = OnceCell::new();
-pub(crate) static SNS_CLIENT: OnceCell<Arc<SNSClient>> = OnceCell::new();
-pub(crate) static SQS_CLIENT: OnceCell<Arc<SQSClient>> = OnceCell::new();
-pub(crate) static PINN_CONFIG: OnceCell<Arc<PinnConfig>> = OnceCell::new();
-pub(crate) static CLI_ARGS: OnceCell<Arc<CLIArgs>> = OnceCell::new();
+pub(crate) static CLUSTER_ENV: OnceCell<AtomicCell<EnvName>> = OnceCell::new();
+pub(crate) static SNS_CLIENT: OnceCell<AtomicCell<SNSClient>> = OnceCell::new();
+pub(crate) static SQS_CLIENT: OnceCell<AtomicCell<SQSClient>> = OnceCell::new();
+pub(crate) static PINN_CONFIG: OnceCell<AtomicCell<PinnConfig>> = OnceCell::new();
+pub(crate) static CLI_ARGS: OnceCell<AtomicCell<CLIArgs>> = OnceCell::new();
 
 // </editor-fold desc="// Global Statics ...">
 
@@ -36,10 +37,10 @@ pub(crate) static CLI_ARGS: OnceCell<Arc<CLIArgs>> = OnceCell::new();
 async fn create_topic<T: AsRef<str>>(topic: T) -> Result<SNSTopicARN, Terminator> {
     println!("Ensuring existence of topic: \"{}\"", topic.as_ref());
 
-    let topic: String = if CLUSTER_ENV.get().unwrap().is_unknown() {
+    let topic: String = if CLUSTER_ENV.get().unwrap().borrow().is_unknown() {
         topic.as_ref().to_string()
     } else {
-        let suffix = CLUSTER_ENV.get().unwrap().as_suffix();
+        let suffix = CLUSTER_ENV.get().unwrap().borrow().as_suffix().to_string();
         println!(
             "Suffixing topic \"{}\" as \"{}-{}\" per in-cluster configuration...",
             topic.as_ref(),
@@ -52,6 +53,7 @@ async fn create_topic<T: AsRef<str>>(topic: T) -> Result<SNSTopicARN, Terminator
     let resp = match SNS_CLIENT
         .get()
         .unwrap()
+        .borrow()
         .create_topic()
         .name(&topic)
         .send()
@@ -84,9 +86,9 @@ async fn create_topic<T: AsRef<str>>(topic: T) -> Result<SNSTopicARN, Terminator
 async fn create_queue<T: AsRef<str>>(queue: T) -> Result<(SQSQueueURL, SQSQueueARN), Terminator> {
     println!("Ensuring existence of queue: \"{}\"", queue.as_ref());
 
-    let suffix = CLUSTER_ENV.get().unwrap().as_suffix();
+    let suffix = CLUSTER_ENV.get().unwrap().borrow().as_suffix().to_string();
 
-    let queue: String = if CLUSTER_ENV.get().unwrap().is_unknown() {
+    let queue: String = if CLUSTER_ENV.get().unwrap().borrow().is_unknown() {
         queue.as_ref().to_string()
     } else {
         println!(
@@ -98,20 +100,19 @@ async fn create_queue<T: AsRef<str>>(queue: T) -> Result<(SQSQueueURL, SQSQueueA
         format!("{}-{}", queue.as_ref(), suffix,)
     };
 
-    let cli_args = CLI_ARGS.get().unwrap();
-
     // If a usable region and account id were provided,
     // set the queue policy to allow any SNS topic in
     // the same region/account/suffix to send messages
     // to this queue
+    let (aws_region, aws_account_id) = match CLI_ARGS.get().unwrap().try_borrow() {
+        Some(args) => (args.aws_region.clone(), args.aws_account_id.clone()),
+        None => (None, None),
+    };
 
-    let policy = if cli_args.aws_region.is_some() && cli_args.aws_account_id.is_some() {
-        let (region, account_id) = (
-            cli_args.aws_region.as_ref().unwrap(),
-            cli_args.aws_account_id.as_ref().unwrap(),
-        );
-        format!(
-            r#"{{
+    let policy: String = match (aws_region, aws_account_id) {
+        (Some(region), Some(account_id)) => {
+            format!(
+                r#"{{
         "Version": "2008-10-17",
         "Statement": [
             {{
@@ -141,14 +142,15 @@ async fn create_queue<T: AsRef<str>>(queue: T) -> Result<(SQSQueueURL, SQSQueueA
             }}
         ]
     }}"#
-        )
-    } else {
-        String::new()
+            )
+        }
+        _ => String::new(),
     };
 
     let resp = match SQS_CLIENT
         .get()
         .unwrap()
+        .borrow()
         .create_queue()
         .queue_name(&queue)
         .attributes(QueueAttributeName::Policy, policy)
@@ -176,6 +178,7 @@ async fn create_queue<T: AsRef<str>>(queue: T) -> Result<(SQSQueueURL, SQSQueueA
     let attributes = SQS_CLIENT
         .get()
         .unwrap()
+        .borrow()
         .get_queue_attributes()
         .queue_url(queue_url.as_str())
         .attribute_names(QueueAttributeName::QueueArn)
@@ -226,6 +229,7 @@ async fn create_subscription<T: AsRef<str>>(queue_arn: T, topic: T) -> Result<u8
     let subscription = match SNS_CLIENT
         .get()
         .unwrap()
+        .borrow()
         .subscribe()
         .topic_arn(&topic_arn)
         .protocol("sqs")
@@ -261,7 +265,7 @@ async fn create_subscription<T: AsRef<str>>(queue_arn: T, topic: T) -> Result<u8
 
 async fn apply_queue_configuration<T: AsRef<str>>(
     queue: T,
-    config: &'static SQSQueueConfig,
+    config: SQSQueueConfig,
 ) -> Result<u8, u8> {
     // Create a convenient place to accumulate
     // the task handles we're about to create
@@ -272,8 +276,9 @@ async fn apply_queue_configuration<T: AsRef<str>>(
         // "unsubscribed", just create the configured topics but
         // don't attempt to subscribe them to anything
         config.topics.iter().for_each(|topic| {
+            let task_topic = topic.to_string();
             tasks.push(tokio::spawn(async {
-                match create_topic(topic.to_string()).await {
+                match create_topic(task_topic).await {
                     Ok(_) => 0,
                     Err(_) => 1,
                 }
@@ -290,11 +295,9 @@ async fn apply_queue_configuration<T: AsRef<str>>(
 
         // Create the queue's required subscriptions
         config.topics.iter().for_each(|topic| {
-            let task_arn = queue_arn.clone();
+            let (task_topic, task_arn) = (topic.to_string(), queue_arn.clone());
             tasks.push(tokio::spawn(async move {
-                create_subscription(task_arn, topic.to_string())
-                    .await
-                    .unwrap()
+                create_subscription(task_arn, task_topic).await.unwrap()
             }));
         })
     }
@@ -336,19 +339,36 @@ async fn main() -> ExitCode {
 
     println!("Applying queue configuration: {:#?}", &pinn_config);
 
-    PINN_CONFIG.set(Arc::new(pinn_config)).unwrap();
-    CLUSTER_ENV.set(Arc::new(env_name)).unwrap();
-    CLI_ARGS.set(Arc::new(args)).unwrap();
+    PINN_CONFIG.set(AtomicCell::new(pinn_config)).unwrap();
+    CLUSTER_ENV.set(AtomicCell::new(env_name)).unwrap();
+    CLI_ARGS.set(AtomicCell::new(args)).unwrap();
 
     // Get a usable AWS configuration objects for the local environment
-    let (sns_config, sqs_config) = match CLI_ARGS.get().unwrap().aws_client_configs().await {
-        Ok((sns, sqs)) => (sns, sqs),
-        Err(error) => {
-            println!(
-                "\n\n{:#?}\n\nCould not create usable AWS configuration due to ^\n\n",
-                error
-            );
-            return ExitCode::from(3);
+    let (sns_config, sqs_config, sts_config) =
+        match CLI_ARGS.get().unwrap().borrow().aws_client_configs().await {
+            Ok((sns, sqs, sts)) => (sns, sqs, sts),
+            Err(error) => {
+                println!(
+                    "\n\n{:#?}\n\nCould not create usable AWS configuration due to ^\n\n",
+                    error
+                );
+                return ExitCode::from(3);
+            }
+        };
+
+    if CLI_ARGS.get().unwrap().borrow().aws_region.is_some()
+        && CLI_ARGS.get().unwrap().borrow().aws_account_id.is_none()
+    {
+        let sts_client: STSClient = STSClient::from_conf(sts_config);
+
+        if let Some(aws_account_id) = sts_client
+            .get_caller_identity()
+            .send()
+            .await
+            .unwrap()
+            .account()
+        {
+            CLI_ARGS.get().unwrap().borrow_mut().aws_account_id = Some(aws_account_id.to_string());
         }
     };
 
@@ -358,17 +378,19 @@ async fn main() -> ExitCode {
 
     // Allow the created clients to be accessed globally safely via
     // the `OnceCell`s created as part of pinnothera's initialization
-    SNS_CLIENT.set(Arc::new(sns_client)).unwrap();
-    SQS_CLIENT.set(Arc::new(sqs_client)).unwrap();
+    SNS_CLIENT.set(AtomicCell::new(sns_client)).unwrap();
+    SQS_CLIENT.set(AtomicCell::new(sqs_client)).unwrap();
 
     // Spawn async tasks to apply the parsed queue & topic configurations
     let tasks: Vec<_> = PINN_CONFIG
         .get()
         .unwrap()
+        .borrow()
         .iter()
         .map(|(queue, queue_config)| {
+            let (task_queue, task_config) = (queue.to_string(), queue_config.clone());
             tokio::spawn(async move {
-                match apply_queue_configuration(queue, queue_config).await {
+                match apply_queue_configuration(task_queue, task_config).await {
                     Ok(value) => value,
                     Err(value) => value,
                 }
